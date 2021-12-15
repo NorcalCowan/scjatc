9af8183bed65f6fef54e935f197e70fbf349c8f8
BrowCow <cow18003@byui.edu>
2021-12-15 13:49:57 -0800

    add info

--- eap/js/main.js
+++ eap/js/main.js
@@ -102,62 +102,36 @@ const jatc = '15105602032';
 
 
 
-
+// Side nav 
 const hamIcon = document.getElementById("hamIcon");
-hamIcon.addEventListener('click', openNav);
+
 function openNav() {
     document.getElementById("Sidenav").style.width = "70%";
 }
+hamIcon.addEventListener('click', openNav);
   
 /* Set the width of the side navigation to 0 */
 const x_symbol = document.getElementById("x_symbol");
-x_symbol.addEventListener('click', closeNav);
+
 function closeNav() {
 document.getElementById("Sidenav").style.width = "0";
 }
-
- 
-const completeLesson = document.getElementById('completeLesson');
-const section_array = document.getElementsByTagName('section');
-function hideSections(){
-    for(let i=0; i<section_array.length; i++){
-        section_array[i].style.display = 'none'; 
-    }
-}
-// function showSections(){
-//     for(let i=0; i<section_array.length; i++){
-//         section_array[i].style.display = 'block'; 
-//     }
-//     // setTimeout(closeNav, 1000);
-// }
-// completeLesson.addEventListener('click', showSections);
-
-
-const onClick = (event) => {
-    console.log(event.srcElement.id);
-    let clickedID = event.srcElement.id;
-    console.log(clickedID);
-    hideSections();
-
-    let section = document.querySelector('#section_' + clickedID);
-    console.log(section);
-    section.style.display = 'block';
-
-    // setTimeout(closeNav, 1000);
-}
-const nav = document.getElementsByClassName('nav');
-for(let i=0; i<nav.length; i++){
-    nav[i].addEventListener('click', onClick);
-}
-
-
-// const homeIcon = document.getElementById('homeIcon');
-// function goHome(){
-//     location.href = "home.html";
-// }
-// homeIcon.addEventListener('click', goHome);
-
-// Event listeners for Nav functions
-hamIcon.addEventListener('click', openNav);
 x_symbol.addEventListener('click', closeNav);
-// completeLesson.addEventListener('click', showSections);
+
+
+
+// Additional Info Request 
+const evacAddInfo_btn = document.getElementById("evacAddInfo_btn");
+
+function openAddInfo() {
+    document.getElementById("evacAddInfo").style.width = "90%";
+}
+evacAddInfo_btn.addEventListener('click', openAddInfo);
+  
+/* Set the width of the side navigation to 0 */
+const evacAddInfoX_symbol = document.getElementById("evacAddInfo-x_symbol");
+
+function closeAddInfo() {
+document.getElementById("evacAddInfo").style.width = "0";
+}
+evacAddInfoX_symbol.addEventListener('click', closeAddInfo);
